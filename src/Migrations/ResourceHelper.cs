025a64d72a4912ef99750f6e38cd7e012667bd74
David Moore <david@robokiwi.com>
2021-12-27 22:53:03 +1300

    Simplified resource helper

--- src/Migrations/ResourceHelper.cs
+++ src/Migrations/ResourceHelper.cs
@@ -7,18 +7,11 @@ namespace Migrations
     {
         public static Stream GetEmbeddedResource(string name)
         {
-            var assembly = Assembly.GetCallingAssembly();
-            var stream = assembly.GetManifestResourceStream(name);
-            if (stream == null)
-                throw new MigrationException(
-                    $"Couldn't load resource named '{name}'. Available resource names: {string.Join("\r\n", assembly.GetManifestResourceNames())}");
-
-            return stream;
+            return GetEmbeddedResource(Assembly.GetCallingAssembly(), name);
         }
 
-        public static Stream GetEmbeddedResource(Assembly assembnly, string name)
+        public static Stream GetEmbeddedResource(Assembly assembly, string name)
         {
-            var assembly = Assembly.GetCallingAssembly();
             var stream = assembly.GetManifestResourceStream(name);
             if (stream == null)
                 throw new MigrationException(
