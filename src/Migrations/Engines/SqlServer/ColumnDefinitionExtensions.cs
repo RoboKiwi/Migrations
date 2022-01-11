bc113f7f5bc7168f0a7a8eabf5d239d0d3521c60
David Moore <david@robokiwi.com>
2022-01-11 20:04:36 +1300

    SQL Server string types (char, nchar, varchar, nvarchar)

---
+++ src/Migrations/Engines/SqlServer/ColumnDefinitionExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using Migrations.Models;
+
+namespace Migrations.Engines.SqlServer;
+
+public static class ColumnDefinitionExtensions
+{
+    public static string ToSqlType(this ColumnDefinition def)
+    {
+        switch (def.SqlType)
+        {
+            case SqlDbType.BigInt: return "bigint";
+
+            case SqlDbType.Binary:
+                break;
+            case SqlDbType.Bit:
+                break;
+            case SqlDbType.Char:
+                return def.Precision switch
+                {
+                    >= 1 and <= 8000 => "char(" + def.Precision + ")",
+                    > 8000 => "char(8000)",
+                    < 1 => "char(1)"
+                };
+
+            case SqlDbType.DateTime:
+                break;
+            case SqlDbType.Decimal:
+                break;
+            case SqlDbType.Float:
+                break;
+            case SqlDbType.Image:
+                break;
+            case SqlDbType.Int:
+                switch (def.Precision)
+                {
+                    case 0:
+                    case 4:
+                        return "int";
+                    case 8:
+                        return "bigint";
+                    case 2:
+                        return "smallint";
+                    case 1:
+                        return "tinyint";
+                }
+
+                break;
+
+            case SqlDbType.Money:
+                break;
+            case SqlDbType.NChar:
+                return def.Precision switch
+                {
+                    >= 1 and <= 4000 => "nchar(" + def.Precision + ")",
+                    > 4000 => "nchar(4000)",
+                    < 1 => "nchar(1)"
+                };
+                
+            case SqlDbType.NText:
+                break;
+            case SqlDbType.NVarChar:
+                return def.Precision is > 0 and <= 4000 ? "nvarchar(" + def.Precision + ")" : "nvarchar(max)";
+
+            case SqlDbType.Real:
+                break;
+            case SqlDbType.UniqueIdentifier:
+                break;
+            case SqlDbType.SmallDateTime:
+                break;
+            case SqlDbType.SmallInt: return "smallint";
+
+            case SqlDbType.SmallMoney:
+                break;
+            case SqlDbType.Text:
+                break;
+            case SqlDbType.Timestamp:
+                break;
+            case SqlDbType.TinyInt: return "tinyint";
+            case SqlDbType.VarBinary:
+                break;
+            case SqlDbType.VarChar:
+                return def.Precision is > 0 and <= 8000 ? "varchar(" + def.Precision + ")" : "varchar(max)";
+
+            case SqlDbType.Variant:
+                break;
+            case SqlDbType.Xml:
+                break;
+            case SqlDbType.Udt:
+                break;
+            case SqlDbType.Structured:
+                break;
+            case SqlDbType.Date:
+                break;
+            case SqlDbType.Time:
+                break;
+            case SqlDbType.DateTime2:
+                break;
+            case SqlDbType.DateTimeOffset:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        throw new NotSupportedException();
+    }
+}
---
+++ tests/Migrations.Tests/Engines/SqlServer/ToSqlTypeTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Data;
+using Migrations.Engines.SqlServer;
+using Migrations.Models;
+using Xunit;
+
+namespace Migrations.Tests.Engines.SqlServer;
+
+public class ToSqlTypeTests
+{
+    [Theory]
+    [InlineData("nchar(1)", SqlDbType.NChar, 0)]
+    [InlineData("nchar(4000)", SqlDbType.NChar, 4001)]
+    [InlineData("nchar(4000)", SqlDbType.NChar, 4000)]
+    [InlineData("nchar(3999)", SqlDbType.NChar, 3999)]
+    [InlineData("nchar(800)", SqlDbType.NChar, 800)]
+    [InlineData("nchar(1)", SqlDbType.NChar, 1)]
+    public void NChar(string expected, SqlDbType type, int precision)
+    {
+        Assert.Equal( expected, new ColumnDefinition("TestColumn", type, precision).ToSqlType() );
+    }
+
+    [Theory]
+    [InlineData("char(1)", SqlDbType.Char, 0)]
+    [InlineData("char(8000)", SqlDbType.Char, 8001)]
+    [InlineData("char(8000)", SqlDbType.Char, 8000)]
+    [InlineData("char(7999)", SqlDbType.Char, 7999)]
+    [InlineData("char(800)", SqlDbType.Char, 800)]
+    [InlineData("char(1)", SqlDbType.Char, 1)]
+    public void Char(string expected, SqlDbType type, int precision)
+    {
+        Assert.Equal( expected, new ColumnDefinition("TestColumn", type, precision).ToSqlType() );
+    }
+
+    [Theory]
+    [InlineData("varchar(max)", SqlDbType.VarChar, 0)]
+    [InlineData("varchar(max)", SqlDbType.VarChar, 8001)]
+    [InlineData("varchar(8000)", SqlDbType.VarChar, 8000)]
+    [InlineData("varchar(7999)", SqlDbType.VarChar, 7999)]
+    [InlineData("varchar(800)", SqlDbType.VarChar, 800)]
+    [InlineData("varchar(1)", SqlDbType.VarChar, 1)]
+    public void Varchar(string expected, SqlDbType type, int precision)
+    {
+        Assert.Equal( expected, new ColumnDefinition("TestColumn", type, precision).ToSqlType() );
+    }
+
+    [Theory]
+    [InlineData("nvarchar(max)", SqlDbType.NVarChar, 0)]
+    [InlineData("nvarchar(max)", SqlDbType.NVarChar, 4001)]
+    [InlineData("nvarchar(4000)", SqlDbType.NVarChar, 4000)]
+    [InlineData("nvarchar(3999)", SqlDbType.NVarChar, 3999)]
+    [InlineData("nvarchar(400)", SqlDbType.NVarChar, 400)]
+    [InlineData("nvarchar(1)", SqlDbType.NVarChar, 1)]
+    public void Nvarchar(string expected, SqlDbType type, int precision)
+    {
+        Assert.Equal( expected, new ColumnDefinition("TestColumn", type, precision).ToSqlType() );
+    }
+
+    [Theory]
+    [InlineData("tinyint", SqlDbType.Int, 1)]
+    [InlineData("tinyint", SqlDbType.TinyInt, 0)]
+    [InlineData("smallint", SqlDbType.Int, 2)]
+    [InlineData("smallint", SqlDbType.SmallInt, 0)]
+    [InlineData("int", SqlDbType.Int, 4)]
+    [InlineData("bigint", SqlDbType.Int, 8)]
+    [InlineData("bigint", SqlDbType.BigInt, 0)]
+    public void Int(string expected, SqlDbType type, int precision)
+    {
+        //-2147483648L, 2147483647L
+        Assert.Equal( expected, new ColumnDefinition("TestColumn", type, precision).ToSqlType() );
+    }
+}
